fd1f3ae9a1160639266c5c51894a9c294c373fa4
shoutaro <850123.a@gmail.com>
2024-03-13 22:04:27 +0900

    8.2.2終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,6 +35,11 @@ module SessionsHelper
     end
   end
 
+   # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
+  end
+
   def logged_in?
     !current_user.nil? # 一時的ｾｯｼｮﾝにuser.idが存在しないじゃないの時true
   end
