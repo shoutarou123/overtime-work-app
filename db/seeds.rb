9a845486f40500490d07c4cc701a019f618bdd55
shoutaro <850123.a@gmail.com>
2024-03-15 14:38:50 +0900

    Finish japanization-and-time-format

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,17 @@ User.create!(name: "管理者",
              password: "password",
              password_confirmation: "password",
              admin: true)
+
+User.create!(name: "職員A",
+             employee_number: 001,
+             base_pay: 1000,
+             password: "password",
+             password_confirmation: "password",
+             )
+
+User.create!(name: "職員B",
+             employee_number: 002,
+             base_pay: 1000,
+             password: "password",
+             password_confirmation: "password",
+             )
