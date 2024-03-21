bffbe0f0732c7d049836734fe04b787037d895aa
shoutaro <850123.a@gmail.com>
2024-03-21 16:39:18 +0900

    時間外勤務申請入力行を1行に変更

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,16 @@
 class AttendancesController < ApplicationController
   
 
-  def edit_overtime_req  #残業申請
+  def edit_overtime_req  # 残業申請画面
     
+  end
+
+  def update_overtime_req # 残業申請送信先
+    if
+    
+    else
+      flash[:danger] = "未入力項目があったため、申請をキャンセルしました。"
+    end
+    redirect_to attendances_edit_overtime_req_users_url
   end
 end
