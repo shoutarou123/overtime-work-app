6b7ce4235d25fa5f2e50725d6e1a761d86094140
shoutaro <850123.a@gmail.com>
2024-04-06 22:12:14 +0900

    それぞれの持っているデータで時間外合計、集計表示するよう修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,5 @@
 module UsersHelper
+  
 
   def unit_price_125(base_pay) # 単価125
     (base_pay*12/1875.5*125/100).round
@@ -21,11 +22,11 @@ module UsersHelper
   end
 
   def sum_h_125
-    Attendance.all.sum(:unit_h_125)
+    @attendances.sum(:unit_h_125)
   end
 
   def sum_m_125
-    Attendance.all.sum(:unit_m_125)
+    @attendances.sum(:unit_m_125)
   end
 
   def sum_125
@@ -35,11 +36,11 @@ module UsersHelper
   end
 
   def sum_h_135
-    Attendance.all.sum(:unit_h_135)
+    @attendances.sum(:unit_h_135)
   end
 
   def sum_m_135
-    Attendance.all.sum(:unit_m_135)
+    @attendances.sum(:unit_m_135)
   end
 
   def sum_135
@@ -49,11 +50,11 @@ module UsersHelper
   end
 
   def sum_h_150
-    Attendance.all.sum(:unit_h_150)
+    @attendances.sum(:unit_h_150)
   end
 
   def sum_m_150
-    Attendance.all.sum(:unit_m_150)
+    @attendances.sum(:unit_m_150)
   end
 
   def sum_150
@@ -63,11 +64,11 @@ module UsersHelper
   end
 
   def sum_h_160
-    Attendance.all.sum(:unit_h_160)
+    @attendances.sum(:unit_h_160)
   end
 
   def sum_m_160
-    Attendance.all.sum(:unit_m_160)
+    @attendances.sum(:unit_m_160)
   end
 
   def sum_160
