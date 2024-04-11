a523193f2f211622cd6a8a9311724f3edebe2700
shoutaro <850123.a@gmail.com>
2024-04-12 05:31:59 +0900

    他のﾕｰｻﾞｰの勤務変更画面に直打ち遷移できないように実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,10 @@ class ApplicationController < ActionController::Base
     redirect_to root_url unless current_user.admin?
   end
 
+  def office_staff_user
+    redirect_to root_url unless current_user.office_staff?
+  end
+
   def set_one_month
     @first_day = params[:date].nil? ?
     Date.current.beginning_of_month : params[:date].to_date
--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,6 +2,7 @@ class AttendancesController < ApplicationController
   before_action :set_user, only: :edit_one_month
   before_action :logged_in_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
+  before_action :office_staff_user, only: [:edit_one_month, :update_one_month]
 
   def edit_one_month
   end
