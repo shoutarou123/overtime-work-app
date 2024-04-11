09573a67b11faa05a34a7f89cd917612a96dea62
shoutaro <850123.a@gmail.com>
2024-04-11 15:11:34 +0900

    庶務係権限のあるそれぞれに承認申請送信されることを確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 # coding: utf-8
 
 User.create!(name: "管理者",
+             base_pay: 0,
              password: "password",
-             base_pay: 305900,
              password_confirmation: "password",
              admin: true)
 
-User.create!(name: "所属長",
+User.create!(name: "山根",
              employee_number: 000,
-             department: "A署",
-             job_title: "署長",
+             department: "T分署",
+             job_title: "分署長",
              base_pay: 500000,
              password: "password",
              password_confirmation: "password",
@@ -18,7 +18,7 @@ User.create!(name: "所属長",
 
 User.create!(name: "職員A",
              employee_number: 001,
-             department: "A署",
+             department: "T分署",
              job_title: "主任",
              base_pay: 305900,
              password: "password",
@@ -26,14 +26,26 @@ User.create!(name: "職員A",
 
 User.create!(name: "職員B",
              employee_number: 002,
-             department: "A署",
+             department: "T分署",
              job_title: "消防士長",
              base_pay: 401200,
              password: "password",
              password_confirmation: "password")
 
-User.create!(name: "庶務係",
-             password: "password",
+User.create!(name: "庶務係さとう",
+             employee_number: 100,
+             department: "T分署",
+             job_title: "消防士長",
              base_pay: 305900,
+             password: "password",
+             password_confirmation: "password",
+             office_staff: true)
+
+User.create!(name: "庶務係すずき",
+             employee_number: 101,
+             department: "T分署",
+             job_title: "消防士",
+             base_pay: 305900,
+             password: "password",
              password_confirmation: "password",
              office_staff: true)
