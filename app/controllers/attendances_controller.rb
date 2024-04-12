f6ead1fa1c3147712fc36f4a36753d35a10ba1f3
shoutaro <850123.a@gmail.com>
2024-04-12 22:25:29 +0900

    勤務変更表示制限、お知らせ表示制限

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,7 +3,7 @@ class AttendancesController < ApplicationController
   before_action :logged_in_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
   before_action :office_staff_user, only: [:edit_one_month, :update_one_month, :edit_overtime_req, :update_overtime_app]
-  before_action :superior_user_and_report_user, only: [:edit_overtime_aprv, :overtime_report]
+  before_action :superior_user_and_report_user, only: [:edit_overtime_aprv, :overtime_report, :edit_chg_req]
 
   def edit_one_month
   end
